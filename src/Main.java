8de3977cbadd03ae855ef7ae78817e6c796608de
eduardo da silva <eduardo.dsilva04@gmail.com>
2024-08-12 10:04:40 -0300

    .

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+                                                public class Main {
+                                                    public static void main(String[] args) {
+                                                        final String nome = "Eduardo da Silva";
+                                                        final String email = "eduardo.dsilva04@gmail.com";
+                                                        final String[] tecnologias = {"JAVA", "PL/SQL", "APEX"};
+                                                    }
+                                                }
+
+
